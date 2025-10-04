d06c72b7e015142845619f6c77f94a64fcaf1d91
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2025-10-04 14:33:08 -0700

    input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float Speed = 5f;
+    private Vector2 _moveInput;
+    private Rigidbody2D _rb;
+    private Camera _mainCamera;
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue value)
     {
-        
+        _moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = _moveInput * Speed;
+        _rb.linearVelocity = velocity;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = transform.position;
+        targetPosition.z = _mainCamera.transform.position.z; // Keep original camera z
+        _mainCamera.transform.position = Vector3.Lerp(
+            _mainCamera.transform.position,
+            targetPosition,
+            0.05f // Smooth factor, adjust as needed
+        );
     }
 }
