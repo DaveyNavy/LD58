4581be7ad1779783e8e1e4cb66d3d293406fbbd0
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2025-10-04 19:19:06 -0700

    aoeenemy

---
+++ Assets/Scripts/AOEEnemy.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class AOEEnemy : Damagable
+{
+    [SerializeField] private float strafeDistance = 10f;
+    private Renderer spriteRenderer;
+    private Camera mainCamera;
+    public int speed;
+
+    [SerializeField] private int aoeCooldown = 200;
+    [SerializeField] private int aoeMeteors = 5;
+    [SerializeField] private GameObject meteorPrefab;
+    private bool inRange;
+    private int aoeTimer = 0;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<Renderer>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (IsSpriteOnScreen())
+        {
+            MoveTowardPlayer();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        aoeTimer = Mathf.Max(aoeTimer - 1, 0);
+        if (aoeTimer == 0 && inRange)
+        {
+            aoeTimer = aoeCooldown;
+        }
+        if (aoeTimer >= aoeCooldown - aoeMeteors * 4 && aoeTimer % 4 == 0)
+        {
+            Instantiate(meteorPrefab, PlayerStats.Instance.playerTransform + (Vector3)(Random.insideUnitCircle * 3f), Quaternion.identity);
+        }
+    }
+
+    #region Effects
+    public float StunTimer;
+
+    #endregion Effects
+
+    void MoveTowardPlayer()
+    {
+        if (StunTimer > 0.1f)
+        {
+            StunTimer -= Time.deltaTime;
+            return;
+        }
+        // Move toward player if far, move away if close:
+        Vector2 toPlayer = PlayerStats.Instance.playerTransform - transform.position;
+        int multiplier = (toPlayer.magnitude < strafeDistance) ?  -1 : 1;
+        inRange = (toPlayer.magnitude < strafeDistance + 2f);
+
+        Vector2 direction = toPlayer.normalized;
+        Vector2 desiredVelocity = direction * speed * multiplier;
+        Vector2 currentVelocity = _rb.linearVelocity;
+        Vector2 velocityChange = desiredVelocity - currentVelocity;
+        _rb.AddForce(velocityChange, ForceMode2D.Force);
+    }
+
+    bool IsSpriteOnScreen()
+    {
+        if (spriteRenderer == null || mainCamera == null)
+        {
+            return false;
+        }
+
+        if (!spriteRenderer.isVisible)
+        {
+            return false;
+        }
+
+        Vector3 screenPoint = mainCamera.WorldToScreenPoint(spriteRenderer.bounds.center);
+
+        bool onScreenX = screenPoint.x > 0 && screenPoint.x < Screen.width;
+        bool onScreenY = screenPoint.y > 0 && screenPoint.y < Screen.height;
+        bool inFrontOfCamera = screenPoint.z > 0;
+
+        return onScreenX && onScreenY && inFrontOfCamera;
+    }
+}
---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    [SerializeField] int Lifetime;
+    [SerializeField] int Damage;
+    [SerializeField] int Knockback;
+    [SerializeField] ParticleSystem blast;
+    private int _lifetimeTimer;
+
+    private void Awake()
+    {
+        _lifetimeTimer = Lifetime;
+        transform.localScale = Vector3.zero;
+    }
+    private void FixedUpdate()
+    {
+        _lifetimeTimer = Mathf.Max(0, _lifetimeTimer - 1);
+        if (_lifetimeTimer == 0) return;
+        float t = 1f - (_lifetimeTimer / (float)Lifetime);
+        float scale = Mathf.Lerp(0f, 3f, t);
+        transform.localScale = Vector3.one * scale;
+        if (_lifetimeTimer == 2)
+        {
+            GetComponent<CircleCollider2D>().enabled = true;
+        }
+        if (_lifetimeTimer == 1) {
+            blast.Play();
+            transform.localScale = Vector3.zero;
+            Destroy(gameObject, 5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<PlayerMovement>();
+        if (player != null)
+        {
+            Vector2 knockback = (PlayerStats.Instance.player.transform.position - transform.position).normalized;
+            player.ApplyKnockback(knockback * Knockback);
+            player.TakeDamage(Damage);
+        }
+    }
+}
