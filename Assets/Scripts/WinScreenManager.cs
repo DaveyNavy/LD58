b78240ec0a48ec1da83ad14fa782ac3b41082b31
kennedy kang <kennedyekang@gmail.com>
2025-10-06 13:53:02 -0700

    win screen

---
+++ Assets/Scripts/WinScreenManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenManager : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("JoScene");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
